dad34e0de4a39ce564f668cd0b51ec08d512b827
Oerlinker <oerlinker@gmail.com>
2026-06-01 20:09:15 -0400

    Fix 0.1.4 fix GCP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tramites-backend-332395305311.us-central1.run.app/api',
-  wsUrl: 'https://tramites-backend-332395305311.us-central1.run.app/ws'
+  apiUrl: 'https://tramites-backend-170902952348.us-east1.run.app/api',
+  wsUrl: 'https://tramites-backend-170902952348.us-east1.run.app/ws',
 };
